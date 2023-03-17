c50c9cbcb9816629fffb931832e587cee8010817
Pozsa Marcell <pozsmarc112@gmail.com>
2023-03-17 19:43:09 +0100

    "befejezve az órai feladat a kiemeles nem jó még."

--- script.js
+++ script.js
@@ -17,32 +17,50 @@ function init() {
   const JOBB_GOMB = document.querySelector(".jobb");
   BAL_GOMB.addEventListener("click", vissza_lep);
   JOBB_GOMB.addEventListener("click", elore_lep);
+  
 
   const KISKEP = document.querySelectorAll("article img");
   console.log(KISKEP);
   for (let i = 0; i < KEPEK.length; i++) {
     KISKEP[i].addEventListener("click", kattintasKezelo);
   }
+  let currentindex = 0;
+  const NAGY_KEP = document.querySelector(".nagykep img");
+  function elore_lep() {
+    if (currentindex < KEPEK.length - 1) {
+      currentindex++;
+      NAGY_KEP.src = KEPEK[currentindex];
+    } else {
+      currentindex = 0;
+      NAGY_KEP.src = KEPEK[currentindex];
+    }
+  }
+  function vissza_lep() {
+    if (currentindex > 0) {
+      currentindex--;
+      NAGY_KEP.src = KEPEK[currentindex];
+    } else if (currentindex == 0) {
+      currentindex = KEPEK.length - 1;
+      NAGY_KEP.src = KEPEK[currentindex];
+    }
+  }
+  function osszehasonlit() {
+    const KISKEP = document.querySelectorAll("article img");
+    KISKEP.forEach((img) => {
+      if (img === selectedImg) {
+        img.classList.add("selected");
+      } else {
+        img.classList.remove("selected");
+      }
+    });
+  }
 }
 
-function elore_lep() {}
-function vissza_lep() {}
 function kattintasKezelo(event) {
   const NAGY_KEP = document.querySelectorAll("section div img");
   console.log(NAGY_KEP);
-  NAGY_KEP[0].src = event.target.src ;
+  NAGY_KEP[0].src = event.target.src;
   console.log(event.target);
   console.log(event.target.src);
+  osszehasonlit(event.target)
 }
-
-const images = document.querySelectorAll(".hoverable");
-
-images.forEach((img) => {
-  img.addEventListener("mouseover", () => {
-    img.style.border = "5px solid lightgreen";
-  });
-
-  img.addEventListener("mouseout", () => {
-    img.style.border = "none";
-  });
-});
